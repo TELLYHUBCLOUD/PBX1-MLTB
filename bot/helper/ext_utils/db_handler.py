02aa2ac80de37231a0c5a40e29f909b0e29570a8
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-11-24 14:07:11 +0530

    Update db_handler.py

--- bot/helper/ext_utils/db_handler.py
+++ bot/helper/ext_utils/db_handler.py
@@ -18,7 +18,7 @@ class DbManger:
     def __connect(self):
         try:
             self.__conn = AsyncIOMotorClient(DATABASE_URL)
-            self.__db = self.__conn.wzmlx # New Section for not conflicting with mltb section !!
+            self.__db = self.__conn[f'telly{bot_id}']  # Fixed: Added f-string
         except PyMongoError as e:
             LOGGER.error(f"Error in DB connection: {e}")
             self.__err = True
@@ -66,32 +66,27 @@ class DbManger:
                 del row['_id']
                 rss_dict[user_id] = row
             LOGGER.info("Rss data has been imported from Database.")
-        self.__conn.close
 
     async def update_deploy_config(self):
         if self.__err:
             return
         current_config = dict(dotenv_values('config.env'))
         await self.__db.settings.deployConfig.replace_one({'_id': bot_id}, current_config, upsert=True)
-        self.__conn.close
 
     async def update_config(self, dict_):
         if self.__err:
             return
         await self.__db.settings.config.update_one({'_id': bot_id}, {'$set': dict_}, upsert=True)
-        self.__conn.close
 
     async def update_aria2(self, key, value):
         if self.__err:
             return
         await self.__db.settings.aria2c.update_one({'_id': bot_id}, {'$set': {key: value}}, upsert=True)
-        self.__conn.close
 
     async def update_qbittorrent(self, key, value):
         if self.__err:
             return
         await self.__db.settings.qbittorrent.update_one({'_id': bot_id}, {'$set': {key: value}}, upsert=True)
-        self.__conn.close
 
     async def update_private_file(self, path):
         if self.__err:
@@ -103,21 +98,18 @@ class DbManger:
             pf_bin = ''
         path = path.replace('.', '__')
         await self.__db.settings.files.update_one({'_id': bot_id}, {'$set': {path: pf_bin}}, upsert=True)
-        if path == 'config.env':
+        if path == 'config__env':  # Fixed: After replace, it becomes 'config__env'
             await self.update_deploy_config()
-        else:
-            self.__conn.close
 
     async def update_user_data(self, user_id):
         if self.__err:
             return
-        data = user_data[user_id]
+        data = user_data[user_id].copy()  # Fixed: Create a copy to avoid modifying original
         if data.get('thumb'):
             del data['thumb']
         if data.get('rclone'):
             del data['rclone']
         await self.__db.users[bot_id].replace_one({'_id': user_id}, data, upsert=True)
-        self.__conn.close
 
     async def update_user_doc(self, user_id, key, path=''):
         if self.__err:
@@ -128,11 +120,10 @@ class DbManger:
         else:
             doc_bin = ''
         await self.__db.users[bot_id].update_one({'_id': user_id}, {'$set': {key: doc_bin}}, upsert=True)
-        self.__conn.close
 
     async def get_pm_uids(self):
         if self.__err:
-            return
+            return []  # Fixed: Return empty list instead of None
         return [doc['_id'] async for doc in self.__db.pm_users[bot_id].find({})]
         
     async def update_pm_users(self, user_id):
@@ -141,44 +132,37 @@ class DbManger:
         if not bool(await self.__db.pm_users[bot_id].find_one({'_id': user_id})):
             await self.__db.pm_users[bot_id].insert_one({'_id': user_id})
             LOGGER.info(f'New PM User Added : {user_id}')
-        self.__conn.close
         
     async def rm_pm_user(self, user_id):
         if self.__err:
             return
         await self.__db.pm_users[bot_id].delete_one({'_id': user_id})
-        self.__conn.close
         
     async def rss_update_all(self):
         if self.__err:
             return
         for user_id in list(rss_dict.keys()):
             await self.__db.rss[bot_id].replace_one({'_id': user_id}, rss_dict[user_id], upsert=True)
-        self.__conn.close
 
     async def rss_update(self, user_id):
         if self.__err:
             return
         await self.__db.rss[bot_id].replace_one({'_id': user_id}, rss_dict[user_id], upsert=True)
-        self.__conn.close
 
     async def rss_delete(self, user_id):
         if self.__err:
             return
         await self.__db.rss[bot_id].delete_one({'_id': user_id})
-        self.__conn.close
 
     async def add_incomplete_task(self, cid, link, tag, msg_link, msg):
         if self.__err:
             return
         await self.__db.tasks[bot_id].insert_one({'_id': link, 'cid': cid, 'tag': tag, 'source': msg_link, 'org_msg': msg})
-        self.__conn.close
 
     async def rm_complete_task(self, link):
         if self.__err:
             return
         await self.__db.tasks[bot_id].delete_one({'_id': link})
-        self.__conn.close
 
     async def get_incomplete_tasks(self):
         notifier_dict = {}
@@ -196,14 +180,18 @@ class DbManger:
                 else:
                     notifier_dict[row['cid']] = {row['tag']: [{row['_id']: row['source']}]}
         await self.__db.tasks[bot_id].drop()
-        self.__conn.close
         return notifier_dict  # return a dict ==> {cid: {tag: [{_id: source}, {_id, source}, ...]}}
 
     async def trunc_table(self, name):
         if self.__err:
             return
         await self.__db[name][bot_id].drop()
-        self.__conn.close
+
+    def close(self):
+        """Properly close the database connection"""
+        if self.__conn:
+            self.__conn.close()
+
 
 if DATABASE_URL:
     bot_loop.run_until_complete(DbManger().db_load())
