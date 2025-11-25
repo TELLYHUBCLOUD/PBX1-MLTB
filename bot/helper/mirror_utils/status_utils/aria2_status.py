dd37bf54ed4343fe46fcd7ca3b1b4cf322034662
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-11-25 12:03:00 +0530

    Update aria2_status.py

--- bot/helper/mirror_utils/status_utils/aria2_status.py
+++ bot/helper/mirror_utils/status_utils/aria2_status.py
@@ -2,19 +2,23 @@
 from time import time
 
 from bot import aria2, LOGGER
-from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_time, sync_to_async
+from bot.helper.ext_utils.bot_utils import (
+    EngineStatus,
+    MirrorStatus,
+    get_readable_time,
+    sync_to_async,
+)
 
 
 def get_download(gid):
     try:
         return aria2.get_download(gid)
     except Exception as e:
-        LOGGER.error(f'{e}: Aria2c, Error while getting torrent info')
+        LOGGER.error(f"❌ {e}: Aria2c, Error while getting torrent info")
         return None
 
 
 class Aria2Status:
-
     def __init__(self, gid, listener, seeding=False, queued=False):
         self.__gid = gid
         self.__download = get_download(gid)
@@ -51,7 +55,7 @@ class Aria2Status:
 
     def eta(self):
         return self.__download.eta_string()
-        
+
     def listener(self):
         return self.__listener
 
@@ -99,21 +103,22 @@ class Aria2Status:
         self.__update()
         await sync_to_async(self.__update)
         if self.__download.seeder and self.seeding:
-            LOGGER.info(f"Cancelling Seed: {self.name()}")
-            await self.__listener.onUploadError(f"Seeding stopped with Ratio: {self.ratio()} and Time: {self.seeding_time()}")
+            LOGGER.info(f"🛑 Cancelling Seed: {self.name()}")
+            msg = f"🛑 <b>Seeding Stopped</b>\n📉 Ratio: {self.ratio()}\n⏳ Time: {self.seeding_time()}"
+            await self.__listener.onUploadError(msg)
             await sync_to_async(aria2.remove, [self.__download], force=True, files=True)
         elif downloads := self.__download.followed_by:
-            LOGGER.info(f"Cancelling Download: {self.name()}")
-            await self.__listener.onDownloadError('Download cancelled by user!')
+            LOGGER.info(f"⛔ Cancelling Download: {self.name()}")
+            await self.__listener.onDownloadError("⛔ Download cancelled by user!")
             downloads.append(self.__download)
             await sync_to_async(aria2.remove, downloads, force=True, files=True)
         else:
             if self.queued:
-                LOGGER.info(f'Cancelling QueueDl: {self.name()}')
-                msg = 'task have been removed from queue/download'
+                LOGGER.info(f"🚫 Cancelling QueueDl: {self.name()}")
+                msg = "⛔ Task has been removed from queue/download"
             else:
-                LOGGER.info(f"Cancelling Download: {self.name()}")
-                msg = 'Download stopped by user!'
+                LOGGER.info(f"⛔ Cancelling Download: {self.name()}")
+                msg = "⛔ Download stopped by user!"
             await self.__listener.onDownloadError(msg)
             await sync_to_async(aria2.remove, [self.__download], force=True, files=True)
 
